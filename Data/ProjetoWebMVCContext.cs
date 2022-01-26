94aa5a3016c34eb5b790dbdd24384fc417a6ff0d
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-26 19:50:03 -0300

    Atualização19:4926012022

---
+++ Data/ProjetoWebMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoWebMVC.Models;
+
+namespace ProjetoWebMVC.Data
+{
+    public class ProjetoWebMVCContext : DbContext
+    {
+        public ProjetoWebMVCContext (DbContextOptions<ProjetoWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoWebMVC.Models.Department> Department { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoWebMVC.Data;
 
 namespace ProjetoWebMVC
 {
@@ -33,6 +35,9 @@ namespace ProjetoWebMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjetoWebMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoWebMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
