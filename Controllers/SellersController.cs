6a12594f2d2726a7ac0492f81d6bf46d08db83a8
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-27 08:28:54 -0300

    Atualização08:2827012022

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
