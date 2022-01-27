7497b345e90f14282411122d3fe647b169c8622d
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-27 14:33:03 -0300

    14:3227012022

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using ProjetoWebMVC.Models;
 using ProjetoWebMVC.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetoWebMVC.Services
 {
@@ -29,7 +30,7 @@ namespace ProjetoWebMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
