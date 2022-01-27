7b99043890127cd9340ded22b687dedc9fb919d2
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-27 11:01:40 -0300

    Atualização11:0127012022

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoWebMVC.Models;
+using ProjetoWebMVC.Models.ViewModels;
 using ProjetoWebMVC.Services;
 
 namespace ProjetoWebMVC.Controllers
@@ -12,10 +13,12 @@ namespace ProjetoWebMVC.Controllers
     {
 
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -26,7 +29,9 @@ namespace ProjetoWebMVC.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using ProjetoWebMVC.Data;
+using ProjetoWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetoWebMVC.Services
+{
+    public class DepartmentService 
+    {
+        private readonly ProjetoWebMVCContext _context;
+
+        public DepartmentService(ProjetoWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
