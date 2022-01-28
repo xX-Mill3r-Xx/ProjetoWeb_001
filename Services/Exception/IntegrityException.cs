d0f5a483964cd5c6c48003baaa518900e718390a
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-28 10:38:13 -0300

    Atualização10:3828012022

---
+++ Services/Exception/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoWebMVC.Services.Exception
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+            
+        }
+    }
+}
