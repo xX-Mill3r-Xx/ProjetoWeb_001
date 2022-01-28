97714326fdadb737166c7132480c156af2f9cce9
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-28 13:37:03 -0300

    Atualização13:3628012022

---
+++ Controllers/SalesRecordsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoWebMVC.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
